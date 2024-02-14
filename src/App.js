c7e0f29e92b4029725a25aeff91b73dfb050057c
aryantongariya <aryantongariya@gmail.com>
2024-02-14 20:23:11 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
      A One Natraj Academy
       </h1>
       <h2>Best Academy of Dance and Gymnastics</h2>
-      <h3>This is academy since 2010</h3>
+      <h3>This academy is  since 2010</h3>
 
     </div>
   );
